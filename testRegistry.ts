92ed6c0a9a6a5d9987244feb6b6123f2d8a748b3
akuan10 <akuan10cc@gmail.com>
2025-04-29 15:41:26 +0800

    呼叫健康實例

--- testRegistry.ts
+++
@@ -1,8 +0,0 @@
-// // testRegistry.ts
-// import { getAllInstances, getHealthyInstances } from './registry';
-
-// // 稍微等待一段時間以確保註冊完畢
-// setTimeout(() => {
-//   console.log('Healthy instances of user-service:', getHealthyInstances('user-service'));
-//   console.log('All instances of user-service:', getAllInstances('user-service'));
-// }, 2000);  // 等待 2 秒鐘後執行
